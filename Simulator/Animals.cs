59daf8a709a41b05696e48e1bef3063949bb70ac
Szymon Cebula <szymon.sc.04@gmail.com>
2024-10-29 18:24:19 +0100

    Settery

--- Simulator/Animals.cs
+++ Simulator/Animals.cs
@@ -4,7 +4,29 @@ namespace Simulator;
 
 public class Animals
 {
-    public required string Description { get; init; }
+    private string description = "Unknown";
+    public required string Description
+    {
+        get => description;
+        init
+        {
+            description = value.Trim();
+            if (description.Length < 3)
+            {
+                description = description.PadRight(3, '#');
+            }
+            if (description.Length > 15)
+            {
+                description = description.Substring(0, 15);
+                description = description.Trim();
+                if (description.Length < 3)
+                {
+                    description = description.PadRight(3, '#');
+                }
+            }
+            description = char.ToUpper(description[0]) + description.Substring(1);
+        }
+    }
     public uint Size { get; set; } = 3;
 
 
--- Simulator/Creature.cs
+++ Simulator/Creature.cs
@@ -2,13 +2,49 @@
 
 internal class Creature
 {
-    public string? Name { get; set; }
-    public int Level { get; set; }
+
+    private string name = "Unknown";
+    public string Name
+    {
+        get => name;
+        init
+        {
+            name = value.Trim();
+
+            if (name.Length < 3)
+            {
+                name = name.PadRight(3, '#');
+            }
+
+            else if (name.Length > 25)
+            {
+                name = name.Substring(0, 25);
+                name = name.Trim();
+                if (name.Length < 3)
+                {
+                    name = name.PadRight(3, '#');
+                }
+            }
+
+            name = char.ToUpper(name[0]) + name.Substring(1);
+        }
+    }
+
+    private int level = 1;
+    public int Level
+    {
+        get => level;
+        init
+        {
+            level = value < 1 ? 1 : value > 10 ? 10 : value;
+        }
+    }
+
 
     public Creature(string name, int level = 1)
     {
         Name = name;
-        Level = level;
+        Level = level >= 1 ? level : 1;
     }
 
     public Creature() { }
@@ -18,5 +54,7 @@ internal class Creature
 
     public void SayHi() =>
         Console.WriteLine($"Hi, I am {Name} and my level is {Level}.");
- 
+
+    public int Upgrade() => level < 10 ? ++level : level;
+
 }
