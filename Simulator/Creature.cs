fad0ad410e0dec7ad2d53b2bcb5d4af9d156ddf9
Szymon Cebula <szymon.sc.04@gmail.com>
2024-10-29 17:40:28 +0100

    Stwory

---
+++ Simulator/Creature.cs
@@ -0,0 +1,22 @@
+﻿namespace Simulator;
+
+internal class Creature
+{
+    public string? Name { get; set; }
+    public int Level { get; set; }
+
+    public Creature(string name, int level = 1)
+    {
+        Name = name;
+        Level = level;
+    }
+
+    public Creature() { }
+
+
+    public string Info => $"{Name} [{Level}]";
+
+    public void SayHi() =>
+        Console.WriteLine($"Hi, I am {Name} and my level is {Level}.");
+ 
+}
