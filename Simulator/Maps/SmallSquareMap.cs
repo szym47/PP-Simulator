455225bb9eea81073c66874b6851131c761ddf99
szym47 <szymon.sc.04@gmail.com>
2024-11-12 22:47:49 +0100

    Mapy

---
+++ Simulator/Maps/SmallSquareMap.cs
@@ -0,0 +1,32 @@
+﻿namespace Simulator.Maps;
+
+public class SmallSquareMap : Map
+{
+    public int Size { get; }
+    private readonly Rectangle bounds;
+    public SmallSquareMap(int size)
+    {
+        if (size < 5 || size > 20)
+            throw new ArgumentOutOfRangeException("Size must be between 5 and 20.");
+
+        Size = size;
+        bounds = new Rectangle(0, 0, Size - 1, Size - 1);
+    }
+
+    public override bool Exist(Point p)
+    {
+        return bounds.Contains(p);
+    }
+
+
+    public override Point Next(Point p, Direction d)
+    {
+        Point nextPoint = p.Next(d);
+        return Exist(nextPoint) ? nextPoint : p;
+    }
+    public override Point NextDiagonal(Point p, Direction d)
+    {
+        Point nextDiagonalPoint = p.NextDiagonal(d);
+        return Exist(nextDiagonalPoint) ? nextDiagonalPoint : p;
+    }
+}
--- Simulator/Program.cs
+++ Simulator/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System.Drawing;
-
+using Simulator.Maps;
 namespace Simulator;
 
 internal class Program
@@ -7,11 +7,15 @@ internal class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Starting Simulator!\n");
+        Console.WriteLine("Point\n");
         Point p = new(10, 25);
         Console.WriteLine(p.Next(Direction.Right));          // (11, 25)
         Console.WriteLine(p.NextDiagonal(Direction.Right));  // (11, 26)
+
         Console.WriteLine();
         Lab5a();
+        Console.WriteLine();
+        Lab5b();
     }
     public static void Lab5a()
     {
@@ -38,9 +42,9 @@ internal class Program
 
             try
             {
-                Point p3 = new Point(5,5);
+                Point p3 = new Point(5, 5);
                 Point p4 = new Point(5, 10);
-                Rectangle rect4 = new Rectangle(p3,p4);
+                Rectangle rect4 = new Rectangle(p3, p4);
                 Console.WriteLine(rect4);
             }
             catch (ArgumentException ex)
@@ -51,8 +55,69 @@ internal class Program
 
             Point testp1 = new Point(7, 8);
             Point testp2 = new Point(20, 20);
-            Console.WriteLine(rect1.Contains(testp1)); 
-            Console.WriteLine(rect1.Contains(testp2)); 
+            Console.WriteLine(rect1.Contains(testp1));
+            Console.WriteLine(rect1.Contains(testp2));
         }
+    }
+    public static void Lab5b()
+    {
+        Console.WriteLine("SmallSquareMap\n");
+
+        {
+            SmallSquareMap map1 = new SmallSquareMap(10);
+
+            Console.WriteLine($"Mapa o rozmiarze {map1.Size} utworzona poprawnie.");
+            try
+            {
+                SmallSquareMap map2 = new SmallSquareMap(21);
+                Console.WriteLine($"Mapa o rozmiarze {map2.Size} utworzona poprawnie.");
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine($"Błąd: {ex.Message}");
+            }
+
+
+            Point insidePoint = new Point(5, 5);
+            Console.WriteLine($"{insidePoint} - {map1.Exist(insidePoint)}");   // true
+
+            Point cornerPoint = new Point(0, 0);
+            Console.WriteLine($"{cornerPoint} - {map1.Exist(cornerPoint)}");  // true
+
+            Point borderPoint = new Point(8, 0);
+            Console.WriteLine($"{borderPoint} - {map1.Exist(borderPoint)}");  // true
+
+            Point outsidePoint = new Point(10, 10);
+            Console.WriteLine($"{outsidePoint} - {map1.Exist(outsidePoint)}");  // false
+            
+            var movedPoint= new Point(0,0);
+            Console.WriteLine("\nRuch w mapie:");
+            Console.WriteLine($"Punkt początkowy: {insidePoint}");
+            movedPoint = map1.Next(insidePoint, Direction.Right);
+            Console.WriteLine($"{insidePoint}, Right -> {movedPoint}");  // (6, 5)
+            movedPoint = map1.Next(insidePoint, Direction.Up);
+            Console.WriteLine($"{insidePoint}, Up -> {movedPoint}");    // (5, 6)
+
+
+            Console.WriteLine("\nRuch poza mapa:");
+            Console.WriteLine($"Punkt początkowy: {cornerPoint}");
+            movedPoint = map1.Next(cornerPoint, Direction.Down);
+            Console.WriteLine($"{cornerPoint}, Down -> {movedPoint}"); // (0 0)
+            movedPoint = map1.Next(cornerPoint, Direction.Left);
+            Console.WriteLine($"{cornerPoint}, Left -> {movedPoint}"); // (0, 0)
+            Console.WriteLine($"Punkt początkowy: {outsidePoint}");
+            movedPoint = map1.Next(outsidePoint, Direction.Left);
+            Console.WriteLine($"{outsidePoint}, Left -> {movedPoint}"); // (10, 10)
+
+
+            Console.WriteLine("\nRuch diagonalny:");
+            Console.WriteLine($"Punkt początkowy: {insidePoint}");
+            movedPoint = map1.NextDiagonal(insidePoint, Direction.Up);
+            Console.WriteLine($"{insidePoint}, Up -> {movedPoint}");  // (6, 6)
+            Console.WriteLine($"Punkt początkowy: {cornerPoint}");
+            movedPoint = map1.NextDiagonal(cornerPoint, Direction.Right);
+            Console.WriteLine($"{cornerPoint}, Right -> {movedPoint}");       // (0,0 )
+        }
+
     }
 }
